5754098071f3e7d92e546205b7827211bad42726
d-murphy <murphy.dan.s@gmail.com>
2020-07-23 19:25:46 -0400

    Update DMportfolio.js

--- DMportfolio.js
+++ DMportfolio.js
@@ -52,3 +52,19 @@ function showSlides2(n) {
   slides[slideIndex2-1].style.display = "block";
   dots[slideIndex2-1].className += " active";
 }
+
+
+var coll = document.getElementsByClassName("collapsible");
+var j;
+
+for (j = 0; j < coll.length; j++) {
+  coll[j].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
