d28b20ded3b4e079ecdc91b0d42b1a85460f30b2
d-murphy <murphy.dan.s@gmail.com>
2020-07-25 11:08:21 -0400

    Update DMportfolio.js

--- DMportfolio.js
+++ DMportfolio.js
@@ -1,10 +1,7 @@
 var coll = document.getElementsByClassName("collapsible");
 var i =0;
-console.log('hit it...this is LA');
-console.log(coll.length);
 
 for (i = 0; i < coll.length; i++) {
-  console.log(i);
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
